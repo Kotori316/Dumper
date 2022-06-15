9931b7d768670dbf9ac37f3359824a3c13656d17
Kotori316 <Kotori316@gmail.com>
2022-06-15 14:33:44 +0900

    Internal changes

--- src/main/scala/com/kotori316/dumper/dumps/FluidNames.scala
+++ src/main/scala/com/kotori316/dumper/dumps/FluidNames.scala
@@ -2,7 +2,8 @@ package com.kotori316.dumper.dumps
 
 import net.minecraft.network.chat.Component
 import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.material.Fluid
+import net.minecraft.world.level.material.{Fluid, Fluids}
+import net.minecraftforge.client.RenderProperties
 import net.minecraftforge.registries.ForgeRegistries
 
 import scala.jdk.CollectionConverters._
@@ -20,9 +21,9 @@ object FluidNames extends FastDumps[Fluid] {
   private[this] final val color = "Color"
   private[this] final val hasBlock = "hasBlock"
   final val formatter = new Formatter[Fluid](Seq("-RegistryName", "-Name", luminosity, density, temperature, viscosity, gaseous, rarity, color, hasBlock),
-    Seq(f => ForgeRegistries.FLUIDS.getKey(f), f => Component.translatable(f.getAttributes.getTranslationKey).getString, _.getAttributes.getLuminosity, _.getAttributes.getDensity,
-      _.getAttributes.getTemperature.toString + " [K]", _.getAttributes.getViscosity, _.getAttributes.isGaseous, _.getAttributes.getRarity.toString,
-      _.getAttributes.getColor.toHexString, _.defaultFluidState().createLegacyBlock() != Blocks.AIR.defaultBlockState))
+    Seq(f => ForgeRegistries.FLUIDS.getKey(f), f => Component.translatable(f.getFluidType.getDescriptionId).getString, _.getFluidType.getLightLevel, _.getFluidType.getDensity,
+      _.getFluidType.getTemperature.toString + " [K]", _.getFluidType.getViscosity, f => f.getFluidType.getDensity < Fluids.WATER.getFluidType.getDensity, _.getFluidType.getRarity.toString,
+      f => RenderProperties.get(f).getColorTint.toHexString, _.defaultFluidState().createLegacyBlock() != Blocks.AIR.defaultBlockState))
 
   override def content(filters: Seq[Filter[Fluid]]): Seq[String] = {
     val fluids = ForgeRegistries.FLUIDS.asScala
